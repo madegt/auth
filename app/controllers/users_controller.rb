a9d9545d2c4dba4550ea0ebc394c8c550bc50c27
Madeleine Garbin <madeleine.garbin@kellogg.northwestern.edu>
2024-02-20 17:19:21 +0000

    wo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     @user = User.new
     @user["first_name"] = params["first_name"]
     @user["last_name"] = params["last_name"]
-    @user["email"] = params["email"]
+    @user["email"] = params["email"] 
     @user["password"] = BCrypt::Password.create(params["password"])
     @user.save
     redirect_to "/users/#{@user["id"]}"
